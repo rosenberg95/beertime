3a7679ea20a52b90d7f7f634d10986f3149fae9e
Poul Rosenberg <poul@poulrosenberg.dk>
2021-10-19 12:41:02 +0200

    just started working - lost track of all the changes

---
+++ src/components/Inputside.vue
@@ -0,0 +1,89 @@
+<template>
+    <v-container>
+    <form @submit.prevent="simuler">
+        <v-layout row>
+            <v-flex xs8>
+                <v-select
+                v-model="select"
+                :items="items"
+                :rules="[v => !!v || 'Item is required']"
+                label="Container Type"
+                required
+                ></v-select>
+            </v-flex>
+        </v-layout>
+        <v-layout row>
+            <v-flex xs8>
+                <v-text-field
+                name="startTemp"
+                label="Start Temperature"
+                id="startTemp"
+                v-model="startTemp"
+                type="float"
+                required
+                :rules="numberRule"
+                > </v-text-field>
+            </v-flex>
+        </v-layout>
+        <v-layout row>
+            <v-flex xs8>
+                <v-text-field
+                name="surrTemp"
+                label="Surrounding Temperature"
+                id="surrTemp"
+                v-model="surrTemp"
+                type="float"
+                required> </v-text-field>
+            </v-flex>
+        </v-layout>
+        <v-layout row>
+            <v-flex xs8>
+                <v-text-field
+                name="targetTemp"
+                label="Target Temperature"
+                id="targetTemp"
+                v-model="targetTemp"
+                type="float"
+                required> </v-text-field>
+            </v-flex>
+        </v-layout>
+        <v-layout row>
+            <v-flex xs8>
+                <v-card-actions>
+                <v-btn type="Submit" color="primary">Calculate</v-btn>
+                </v-card-actions>
+            </v-flex>
+        </v-layout>
+    </form>
+    </v-container>
+</template>
+
+
+
+<script>
+
+export default {
+    name: "Input",
+    data() {
+        return {
+            select: null,
+            items: [
+                'Small can (0.25 L)',
+                'Medium can (0.33 L)',
+                'Small bottle (0.33 L)',
+                'Large bottle (dont know u fuck)',
+            ],
+            numberRule: [v => (!isNaN(parseFloat(v)) && v >= -273.15 && v <= 5500) || 'Number has to be between absolute zero and the surface temperature of the Sun'],
+        }
+    },
+    computed: {
+  
+    },
+    methods: {
+        simuler () {
+            console.log(this.startTemp)
+        }
+    }
+
+}
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+      <v-container>
+      <v-layout row wrap justify-center align-center > 
+        <v-flex xs16 sm8 md6 lg4>
+            <v-card class="ma-3" max-width="800px" height=600px>
+                <div>
+                    <v-card-title><v-icon left>mdi-folder-text</v-icon>Input </v-card-title>
+                    <v-card-subtitle><em>To calculate the time it takes for your beer to cool down from a start temperature to the desired target temperature, please fill out all the fields below. The temperature has to be in degrees Celcius, because fuck Fahrenheit you dumb freedomunit-using noob. We btw assume atmospherical pressure.</em></v-card-subtitle>
+                    <v-card-text center>
+                        <Inputside/>
+                    </v-card-text>
+                    
+                </div>
+            </v-card> 
+        </v-flex>
+        <v-flex xs16 sm8 md6 lg4>
+            <v-card class="ma-3" max-width="800px" height=600px>
+                <div>
+                    <v-card-title><v-icon left>mdi-chart-box</v-icon>Output</v-card-title>
+                    <v-card-text>
+                        <div class="my-4 text-subtitle-1" height="200px">
+                        her står lidt text
+                        </div>
+                    </v-card-text>
+                </div>
+            </v-card> 
+        </v-flex>
+      </v-layout>
+  </v-container>
+  </v-container>
+</template>
+
+
+
+
+
+<script>
+
+  import Inputside from './Inputside.vue'
+
+  export default {
+    name: 'Main',
+    
+    components: {
+      Inputside,
+    },
+
+
+    data: () => ({
+      
+    })
+
+  }
+</script>
