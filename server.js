f2f90ced87fa724785e7453cba2fecabbe966ff9
Poul Rosenberg <poul@poulrosenberg.dk>
2021-10-19 21:07:35 +0200

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
+var PORT = 8080
 
 app = express();
 app.use(serveStatic(path.join(__dirname,'dist')));
-const port = port.env.PORT || 8080;
 
-app.listen(port);
+app.listen(process.env.PORT || PORT);
